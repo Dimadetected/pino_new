75eba18e0238218d88fa8d6d786afaabf745e0e4
dmitry <dimadetected@yandex.ru>
2021-01-20 10:05:46 +0300

    fixFileName

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,29 +4,28 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    
+
     <title>{{ config('app.name', 'Laravel') }}</title>
-    
+
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    
+
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    
+    <link rel="stylesheet" href="{{ asset('css/app.css' . '?v='. time()) }}">
     @livewireStyles
-    
+
     <!-- Scripts -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.js" defer></script>
-    
+
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 </head>
 <body class="font-sans antialiased">
 <div class="bg-gray-100" style="min-height: 90vh">
     @livewire('navigation-dropdown')
-    
+
     <!-- Page Heading -->
     <header class="bg-white shadow">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -34,7 +33,7 @@
             {!! isset($action) ? $action:"" !!}
         </div>
     </header>
-    
+
     <!-- Page Content -->
     <main class="py-3">
         <div id="app">
@@ -46,7 +45,7 @@
 
 @stack('modals')
 @yield('script')
-<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ asset('js/app.js' . '?v='. time()) }}"></script>
 
 @livewireScripts
 </body>

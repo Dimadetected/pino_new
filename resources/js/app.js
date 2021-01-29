b9493174f9e15ead90fce429b65cbde7f7057491
dmitry <dimadetected@yandex.ru>
2021-01-29 11:17:30 +0300

    fixe liveware trouble

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,6 @@ import Vue from 'vue';
 require('jquery-ui')
 require('jquery-mask-plugin/dist/jquery.mask.min')
 
-
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'

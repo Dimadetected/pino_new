86851d62849856df6583b4938199c246317ed2a0
Дима <dimadetected@yandex.ru>
2022-08-09 18:03:04 +0300

    change tgbot key in env

--- app/Http/Services/TgService.php
+++ app/Http/Services/TgService.php
@@ -34,9 +34,8 @@ class TgService
      */
     public function setWebHook()
     {
-//        $url = 'https://xn--80aqpccbc.xn--p1ai/' . $this->token . '/webhook';
-        $url = 'https://xn--h1akdb.xn--p1acf/' . $this->token . '/webhook';
-        $response = Http::get($this->url . '/setWebhook?remove', [
+        $url = 'https://xn--80aqpccbc.xn--p1ai/' . $this->token . '/webhook';
+         $response = Http::get($this->url . '/setWebhook?remove', [
             'url' => $url
         ]);
         dd(dd($response->body()));

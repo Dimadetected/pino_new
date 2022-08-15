f35dbd338ce2c040d064a5f48816ed65112e424d
Дима <dimadetected@yandex.ru>
2022-08-15 10:07:22 +0300

    read_only

--- resources/views/applications/admin/view.blade.php
+++ resources/views/applications/admin/view.blade.php
@@ -101,7 +101,7 @@
                         </div>
                     @elseif((($bill->user_role_id == $user->user_role_id ) or
                                 ($bill->steps == 0 and $bill->user_id == auth()->user()->id) or
-                                ($bill->user_role_id == $user->user_role_id and $bill->user_role_id == 4)) and (auth()->user()->read_only === false))
+                                ($bill->user_role_id == $user->user_role_id and $bill->user_role_id == 4)) and (auth()->user()->read_only == false))
                         <bill-status-change-component :user_id="{{auth()->user()->id}}" :bill="{{$bill}}"></bill-status-change-component>
                     @endif
                     @if(isset($bill->bill_action->user_id) and ($bill->bill_action->user_id == $user->id) and ($bill->user_role_id != $user->user_role_id))
